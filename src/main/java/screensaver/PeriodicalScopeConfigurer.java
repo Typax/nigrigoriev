b18350806a1125b5d6b7a0bce818d041ff241060
NGrigoriev <ngrigoriev@lanit.ru>
2019-02-19 20:28:16 +0300

    самодельный Scope доделал

--- src/main/java/screensaver/PeriodicalScopeConfigurer.java
+++ src/main/java/screensaver/PeriodicalScopeConfigurer.java
@@ -1,16 +1,35 @@
 package screensaver;
 
+import javafx.util.Pair;
 import org.springframework.beans.factory.ObjectFactory;
 import org.springframework.beans.factory.config.Scope;
+
+
+import java.time.LocalTime;
+import java.util.HashMap;
+import java.util.Map;
+
+import static java.time.LocalTime.now;
+
 
 /**
  * Добавлен новый Scope для того, чтобы беан изменялся не каждый раз когда запрашивается (цвет)
  * а через 3 секунды
  */
 public class PeriodicalScopeConfigurer implements Scope {
+    Map<String, Pair<LocalTime, Object>> map = new HashMap<>();
     @Override
-    public Object get(String s, ObjectFactory<?> objectFactory) {
-        return null;
+    public Object get(String name, ObjectFactory<?> objectFactory) {
+        if (map.containsKey(name)) {
+            Pair<LocalTime, Object> pair = map.get(name);
+            int secondsSinceLastRequest  = now().getSecond() - pair.getKey().getSecond();
+            if (secondsSinceLastRequest > 3) {
+                map.put(name, new Pair(now(), objectFactory.getObject()));
+            }
+        } else {
+            map.put(name, new Pair(now(), objectFactory.getObject()));
+        }
+        return map.get(name).getValue();
     }
 
     @Override
